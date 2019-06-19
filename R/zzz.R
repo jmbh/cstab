6dc6b587b688e53c402984eefe4614a65c03d622
Dirk Wulff <dirkwulff@fp-ws-b210.psycho.unibas.ch>
2019-06-19 14:01:16 +0200

    new correction

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,7 @@
+#' @useDynLib cstab
+#' @importFrom Rcpp sourceCpp
+NULL
+
 .onLoad <- function(libname, pkgname) {
   assign('lkup',lookup(n = 10000, root = 200),envir = parent.env(environment()))
   }
