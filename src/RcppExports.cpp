f6813e8daa68a115ae0bdee686fa0066280b8c87
Dirk Wulff <dwulff@users.noreply.github.com>
2023-10-01 19:29:43 +0200

    fixed package help

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // fast_dist
 NumericMatrix fast_dist(NumericMatrix x, int power);
 RcppExport SEXP _cstab_fast_dist(SEXP xSEXP, SEXP powerSEXP) {
